cd8180fc99b456592a4796649415c90c6c644011
Maverick Tesini <mavericktesini@gmail.com>
2018-07-12 21:18:34 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ class App < Sinatra::Base
 
   # Define your GET '/' route below and respond with "Hello, World!"
   get "/" do
-    "Here's Johnny!!!"
+    "Here's Johnny!!! Playtime!"
   end
 
 end
